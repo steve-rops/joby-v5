359957d863fec286e2f97f5c4e55db0195583555
steve-rops <somebiss@gmail.com>
2025-08-11 18:01:23 +0300

    added the delete liked functionality

--- src/dashboard/liked/Liked.jsx
+++ src/dashboard/liked/Liked.jsx
@@ -1,18 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { useDeleteJobFromMetadata } from "@/hooks/user/useDeleteJobFromMetadata";
 import { useUser } from "@clerk/clerk-react";
+import { Trash, Trash2 } from "lucide-react";
 import { Link } from "react-router";
 
 export const Liked = () => {
   const { user } = useUser();
+  const { mutate: deleteJob } = useDeleteJobFromMetadata();
   const { liked } = user?.unsafeMetadata || [];
-
-  console.log(liked);
 
   return (
     <div className="space-y-4">
       {liked.map((job) => (
-        <div className="border p-2 rounded-lg space-y-2 ">
-          <div className="text-lg">{job.title}</div>
+        <div key={job.id} className="border p-4 rounded-lg space-y-4 ">
+          <div className="flex justify-between gap-4">
+            <div className="text-lg">{job.title}</div>
+            <div>
+              <Trash2
+                onClick={() => deleteJob(job.id)}
+                className="text-red-600 w-5 h-5"
+              />
+            </div>
+          </div>
           <Link to={job.url} target="_blank">
             <Button className="w-full text-xs" size="sm">
               Learn More
