498e6a00a403d68d1ca4e793d0204a80572368fa
steve-rops <somebiss@gmail.com>
2025-10-06 21:17:15 +0300

    added the about section

--- src/components/PagesHeader.jsx
+++ src/components/PagesHeader.jsx
@@ -1,20 +1,37 @@
-import { Menu, Sun, X } from "lucide-react";
+import { ArrowUp, Menu, Sun, X } from "lucide-react";
 import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Button } from "./ui/button";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import { BubbleDropdown } from "../pages/home/components/BubbleDropdown";
 
 export const PagesHeader = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
   const [showMenuBarCTA, setShowMenuBarCTA] = useState(false);
+  const [isAtLoginPage, setIsAtLoginPage] = useState(false);
+
+  const location = useLocation();
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  const sctollToSection = (id) => {
+    const element = document.getElementById(id);
+    element?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    const pathname = location.pathname;
+    setIsAtLoginPage(pathname.includes("login") || pathname.includes("signup"));
+  }, [location]);
 
   // Toggle scrolled state when passing threshold
   useEffect(() => {
     const handleScroll = () => {
       setScrolled(window.scrollY > 200);
-      setShowMenuBarCTA(window.scrollY > 600);
+      setShowMenuBarCTA(window.scrollY > 800);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -28,41 +45,43 @@ export const PagesHeader = () => {
       <div className="relative">
         <div className="flex items-center  gap-2">
           {/* Menu Button */}
-          <button onClick={handleClick} className="p-2">
-            {isOpen ? <X size={28} /> : <Menu size={28} />}
-          </button>
+
+          {!isAtLoginPage && (
+            <button onClick={handleClick} className="p-2">
+              {isOpen ? <X size={28} /> : <Menu size={28} />}
+            </button>
+          )}
 
           {/* Logo */}
-          <a>
+          <Link to="/">
             <img src="/logo.svg" width={100} height={100} />
-          </a>
+          </Link>
         </div>
 
+        {/* top menu */}
         <AnimatePresence>
           {isOpen && (
             <motion.nav
+              onClick={() => setIsOpen((open) => !open)}
               key="dropdown-nav"
               initial={{ opacity: 0, y: -20 }} // Start above and invisible
               animate={{ opacity: 1, y: 0 }} // Slide down to normal position
               exit={{ opacity: 0, y: -20 }} // Slide up and disappear on close
               transition={{ duration: 0.3, ease: "easeInOut" }}
-              className="overflow-hidden  absolute bg-white/90 backdrop-blur-lg shadow-lg rounded-md"
+              className="overflow-hidden  absolute bg-white/90 backdrop-blur-lg shadow-lg rounded-md z-50"
             >
-              <ul className="flex flex-col gap-3 text-md font-medium  p-4 rounded-md ">
-                <li>
-                  <a href="#home" className="hover:text-blue-500">
-                    Home
-                  </a>
+              <ul className="flex flex-col gap-3 text-md font-medium  p-4 rounded-md z-50 ">
+                <li
+                  onClick={() => sctollToSection("about")}
+                  className="hover:text-blue-500 transition-colors hover:cursor-pointer"
+                >
+                  About
                 </li>
-                <li>
-                  <a href="#about" className="hover:text-blue-500">
-                    About
-                  </a>
-                </li>
-                <li>
-                  <a href="#services" className="hover:text-blue-500">
-                    Services
-                  </a>
+                <li
+                  onClick={() => sctollToSection("whyChooseUs")}
+                  className="hover:text-blue-500 transition-colors hover:cursor-pointer"
+                >
+                  Why us
                 </li>
                 <li>
                   <a href="#contact" className="hover:text-blue-500">
@@ -117,33 +136,26 @@ export const PagesHeader = () => {
             {/* NAV (takes center, expands when logo/cta changes) */}
             {/* bg-screens menu */}
             <motion.nav layout className="hidden md:flex flex-1">
-              <ul className="flex gap-6 text-md font-medium justify-center whitespace-nowrap">
+              <ul className="flex gap-6 items-center text-md font-medium justify-center whitespace-nowrap">
                 {showMenuBarCTA && (
                   <li>
-                    <a
-                      href="#home"
-                      className="hover:text-blue-500 transition-colors"
-                    >
-                      Home
-                    </a>
+                    <Button variant="ghost" onClick={scrollToTop}>
+                      <ArrowUp />
+                    </Button>
                   </li>
                 )}
 
-                <li>
-                  <a
-                    href="#about"
-                    className="hover:text-blue-500 transition-colors"
-                  >
-                    About
-                  </a>
+                <li
+                  onClick={() => sctollToSection("about")}
+                  className="hover:text-blue-500 transition-colors hover:cursor-pointer"
+                >
+                  About
                 </li>
-                <li>
-                  <a
-                    href="#services"
-                    className="hover:text-blue-500 transition-colors"
-                  >
-                    Services
-                  </a>
+                <li
+                  onClick={() => sctollToSection("whyChooseUs")}
+                  className="hover:text-blue-500 transition-colors hover:cursor-pointer"
+                >
+                  Why us
                 </li>
                 <li>
                   <a
--- src/pages/home/components/Hero.jsx
+++ src/pages/home/components/Hero.jsx
@@ -3,13 +3,16 @@ import { Button } from "../../../components/ui/button";
 import { motion } from "framer-motion";
 import { Link } from "react-router";
 import { ArrowRight, Circle, CircleCheck, CircleCheckBig } from "lucide-react";
+import { FaReact } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiShadcnui } from "react-icons/si";
 
 export const Hero = () => {
   const { isSignedIn } = useUser();
 
   return (
-    <section className="p-4 space-y-4 overflow-hidden">
-      <div className="pt-8 flex gap-4 flex-col justify-center items-center">
+    <section className="p-4 overflow-hidden">
+      <div className="py-1 flex gap-4 flex-col justify-center items-center ">
         {/* Badge */}
         <motion.div
           initial={{ opacity: 0, y: -20 }}
@@ -45,12 +48,12 @@ export const Hero = () => {
             Discover jobs near you in seconds with our interactive map. Explore
             with ease, apply in a tap—and yes, it’s completely free.
           </h2>
-          <div className="flex  justify-center items-center gap-6 mt-2">
-            <div className="flex justify-center items-center gap-2 mt-2 text-sm text-muted-foreground/75">
+          <div className="flex  justify-center items-center gap-6 ">
+            <div className="flex justify-center items-center gap-2 my-1  text-sm text-muted-foreground/75">
               <CircleCheckBig size={12} />
               <div>100% free</div>
             </div>
-            <div className="flex justify-center items-center gap-2 mt-2 text-sm text-muted-foreground/75">
+            <div className="flex justify-center items-center gap-2 my-1 text-sm text-muted-foreground/75">
               <CircleCheckBig size={12} />
               <div>No credit card required</div>
             </div>
@@ -62,7 +65,7 @@ export const Hero = () => {
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.6, delay: 0.8 }}
-          className="w-full flex flex-col md:flex-row gap-2 justify-center py-8 mb-22"
+          className="w-full flex flex-col md:flex-row gap-2 items-center justify-center py-4"
         >
           <Link to="/dashboard/search">
             <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
@@ -93,23 +96,50 @@ export const Hero = () => {
             </motion.div>
           </Link>
         </motion.div>
-      </div>
 
-      {/* Hero Image */}
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8, delay: 1 }}
-      >
-        <motion.img
-          src="/plagia.png"
-          width={400}
-          height={200}
-          className="mx-auto rounded-lg"
-          whileHover={{ scale: 1.03 }}
-          transition={{ type: "spring", stiffness: 200 }}
-        />
-      </motion.div>
+        {/* crafted with */}
+        <motion.div
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 1 }}
+          className="flex flex-col justify-center items-center gap-2 py-2 "
+        >
+          <div className="text-center text-sm text-muted-foreground/75 ">
+            Crafted with state the of the art technologies
+          </div>
+          <div className="flex justify-center items-center gap-2 ">
+            <div className="flex flex-col justify-center items-center hover:text-[#59C9F1] hover:scale-120 transition-all">
+              <FaReact className="w-10 h-10 hover:text-[#59C9F1] text-[#59C9F1] lg:text-muted-foreground  " />
+              <div className="text-xs ">React</div>
+            </div>
+            <div className="flex flex-col justify-center items-center hover:text-[#00BCFF] hover:scale-120 transition-all">
+              <RiTailwindCssFill className="w-10 h-10 text-[#00BCFF] lg:text-muted-foreground transition-all hover:text-[#00BCFF]" />
+              <div className="text-xs ">Tailwindcss</div>
+            </div>
+            <div className="flex flex-col  justify-center items-center hover:text-[#000000] hover:scale-120 transition-all">
+              <SiShadcnui className="w-10 h-10  bg-black text-white p-1 rounded-xs" />
+              <div className="text-xs ">ShadcnUI</div>
+            </div>
+          </div>
+        </motion.div>
+
+        {/* Hero Image */}
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 1 }}
+          className="py-4"
+        >
+          <motion.img
+            src="/plagia.png"
+            width={400}
+            height={200}
+            className="mx-auto rounded-lg"
+            whileHover={{ scale: 1.03 }}
+            transition={{ type: "spring", stiffness: 200 }}
+          />
+        </motion.div>
+      </div>
     </section>
   );
 };
